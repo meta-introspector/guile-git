2b8cd571d1e9c95d942b571bbcb86c7c9efe3a43
Ludovic Courtès <ludo@gnu.org>
2021-01-29 15:29:53 +0100

    maint: Specify 'source' field in 'guix.scm'.
    * guix.scm: Add 'source' field.

--- guix.scm
+++ guix.scm
@@ -1,6 +1,8 @@
 
 
 (use-modules ((guix licenses) #:select (gpl3+))
+             (guix gexp)
+             ((guix git-download) #:select (git-predicate))
              (guix packages)
              (guix build-system gnu)
              (gnu packages autotools)
@@ -12,10 +14,15 @@
              (gnu packages tls)
              (gnu packages version-control))
 
+(define %srcdir
+  (dirname (current-filename)))
+
 (package
   (name "guile-git")
   (version "0.1")
-  (source #f)
+  (source (local-file "." "guile-git-checkout"
+                      #:recursive? #t
+                      #:select? (git-predicate %srcdir)))
   (build-system gnu-build-system)
   (native-inputs
    `(("autoconf" ,autoconf)
