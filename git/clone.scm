meta-introspector/guile-git

commit 418ea57d024bda89daf190f141e603a31d167ede

fetch: Consolidate 'init-auth-fetch-options' and 'make-fetch-options'.
The idea here is to initialize FETCH-OPTIONS with the correct
authentication parameters and pass that along, instead of
special-handling authenticated checkouts.

* git/fetch.scm (init-auth-fetch-options): Rename to ...
(make-fetch-options): ... this.  Remove mandatory FETCH-OPTIONS
argument.  Remove previous MAKE-FETCH-OPTIONS procedure.
* git/remote.scm (remote-fetch): Remove #:auth-method key and related
code.
* git/clone.scm (clone): Likewise.
(make-clone-options): Add #:fetch-options key.
* git/structs.scm (clone-options-fetch-options): Remove procedure.
(set-clone-options-fetch-opts!): New procedure.
* tests/clone.scm (clone-test,
"clone-and-fetch-auth-ssh-credentials"): Use #:fetch-options instead
of #:auth-method.

git/clone.scm
@@ -1,6 +1,7 @@
 ;;; Guile-Git --- GNU Guile bindings of libgit2
 ;;; Copyright © 2016 Amirouche Boubekki <amirouche@hypermove.net>
 ;;; Copyright © 2016 Erik Edrosa <erik.edrosa@gmail.com>
+;;; Copyright © 2019 Marius Bakke <marius@devup.no>
 ;;;
 ;;; This file is part of Guile-Git.
 ;;;
@@ -36,27 +37,25 @@
 (define clone
   (let ((proc (libgit2->procedure* "git_clone" '(* * * *))))
     (lambda* (url directory
-                  #:optional (clone-options (make-clone-options))
-                  #:key (auth-method #f))
+                  #:optional (clone-options (make-clone-options)))
       "Clones a remote repository found at URL into DIRECTORY.  An
 authentication method from (git auth) can be passed optionally if the
 repository is protected.  Returns the repository on success or throws an error
 on failure."
-      (let* ((out (make-double-pointer))
-             (fetch-options
-              (clone-options-fetch-options clone-options)))
-        (init-auth-fetch-options fetch-options auth-method)
+      (let ((out (make-double-pointer)))
         (proc out
               (string->pointer url)
               (string->pointer directory)
               (clone-options->pointer clone-options))
         (pointer->repository! (dereference-pointer out))))))
 
-(define (make-clone-options)
+(define make-clone-options
   (let ((proc (libgit2->procedure* "git_clone_init_options" `(* ,unsigned-int)))
         (clone-options (make-clone-options-bytestructure)))
-    (proc (clone-options->pointer clone-options) CLONE-OPTIONS-VERSION)
-    clone-options))
+    (lambda* (#:key (fetch-options (make-fetch-options)))
+      (proc (clone-options->pointer clone-options) CLONE-OPTIONS-VERSION)
+      (set-clone-options-fetch-opts! clone-options fetch-options)
+      clone-options)))
 
 (define clone-init-options
   ;; Deprecated alias for compatibility with 0.2.

tests/clone.scm
@@ -36,7 +36,8 @@
          (clone-dir (in-vicinity repo-dir "out")))
     (clone (make-ssh-url repo-dir ssh-server-port)
            clone-dir
-           #:auth-method auth-method)
+           (make-clone-options #:fetch-options
+                               (make-fetch-options auth-method)))
     (let* ((repository (repository-open clone-dir))
            (oid (reference-target (repository-head repository))))
       (oid->string (commit-id (commit-lookup repository oid))))))
@@ -63,7 +64,7 @@
                 (clone-dir (in-vicinity directory "out"))
                 (repository (repository-open clone-dir))
                 (remote (remote-lookup repository "origin")))
-           (remote-fetch remote #:auth-method auth)
+           (remote-fetch remote #:fetch-options (make-fetch-options auth))
            #t)))))
 
 (libgit2-shutdown!)

git/fetch.scm
@@ -1,5 +1,6 @@
 ;;; Guile-Git --- GNU Guile bindings of libgit2
 ;;; Copyright © 2017, 2019 Mathieu Othacehe <m.othacehe@gmail.com>
+;;; Copyright © 2019 Marius Bakke <marius@devup.no>
 ;;;
 ;;; This file is part of Guile-Git.
 ;;;
@@ -25,8 +26,7 @@
   #:use-module (git types)
   #:use-module (srfi srfi-26)
 
-  #:export (init-auth-fetch-options
-            make-fetch-options
+  #:export (make-fetch-options
             fetch-init-options   ;deprecated!
             set-fetch-auth-with-ssh-agent!
             set-fetch-auth-with-ssh-key!
@@ -34,21 +34,18 @@
 
 (define FETCH-OPTIONS-VERSION 1)
 
-(define init-auth-fetch-options
+(define make-fetch-options
   (let ((proc (libgit2->procedure* "git_fetch_init_options"
                                    `(* ,unsigned-int))))
-    (lambda* (fetch-options #:optional auth-method)
-      (proc (fetch-options->pointer fetch-options) FETCH-OPTIONS-VERSION)
-      (cond
-       ((auth-ssh-credentials? auth-method)
-        (set-fetch-auth-with-ssh-key! fetch-options auth-method))
-       ((auth-ssh-agent? auth-method)
-        (set-fetch-auth-with-ssh-agent! fetch-options)))
-      fetch-options)))
-
-(define* (make-fetch-options #:optional auth-method)
-  (let ((fetch-options (make-fetch-options-bytestructure)))
-    (init-auth-fetch-options fetch-options auth-method)))
+    (lambda* (#:optional auth-method)
+      (let ((fetch-options (make-fetch-options-bytestructure)))
+        (proc (fetch-options->pointer fetch-options) FETCH-OPTIONS-VERSION)
+        (cond
+         ((auth-ssh-credentials? auth-method)
+          (set-fetch-auth-with-ssh-key! fetch-options auth-method))
+         ((auth-ssh-agent? auth-method)
+          (set-fetch-auth-with-ssh-agent! fetch-options)))
+        fetch-options))))
 
 (define fetch-init-options
   ;; Deprecated alias for compatibility with 0.2.

git/remote.scm
@@ -102,9 +102,7 @@
   (let ((proc (libgit2->procedure* "git_remote_fetch" '(* * * *))))
     (lambda* (remote #:key
                      (reflog-message "")
-                     (fetch-options (make-fetch-options))
-                     (auth-method #f))
-      (init-auth-fetch-options fetch-options auth-method)
+                     (fetch-options (make-fetch-options)))
       (proc (remote->pointer remote)
             ;; FIXME https://libgit2.github.com/libgit2/#HEAD/type/git_strarray
             %null-pointer

git/structs.scm
@@ -53,7 +53,7 @@
             fetch-options-download-tags set-fetch-options-download-tags!
             set-fetch-options-callbacks! set-remote-callbacks-credentials!
 
-            make-clone-options-bytestructure clone-options-bytestructure clone-options->pointer clone-options-fetch-options
+            make-clone-options-bytestructure clone-options-bytestructure clone-options->pointer set-clone-options-fetch-opts!
 
             make-describe-options-bytestructure describe-options->pointer describe-options->bytestructure
             set-describe-options-max-candidates-tag! set-describe-options-strategy!
@@ -465,16 +465,11 @@ tag policy in FETCH-OPTIONS."
 (define (clone-options->pointer clone-options)
   (bytestructure->pointer (clone-options-bytestructure clone-options)))
 
-(define (clone-options-fetch-options clone-options)
-  (let* ((fetch-options-bs
-          (bytestructure-ref
-           (clone-options-bytestructure clone-options) 'fetch-opts))
-         (fetch-options-offset (bytestructure-offset fetch-options-bs))
-         (fetch-options-pointer (bytevector->pointer
-                                 (bytestructure-bytevector fetch-options-bs)
-                                 fetch-options-offset)))
-    (%make-fetch-options
-     (pointer->bytestructure fetch-options-pointer %fetch-options))))
+(define (set-clone-options-fetch-opts! clone-options fetch-options)
+  (let ((clone-options-bs (clone-options-bytestructure clone-options))
+        (fetch-options-bs (fetch-options-bytestructure fetch-options)))
+    (bytestructure-set! clone-options-bs 'fetch-opts
+                        (bytestructure-bytevector fetch-options-bs))))
 
 ;; git remote head
 

commit 6ebd95d1d625e78ddcc6cff0ba4d34ceb6059d93

clone: 'make-clone-options' returns fresh clone options.
Until now it would always return the same record.  Regression introduced
in 418ea57d024bda89daf190f141e603a31d167ede.

* git/clone.scm (make-clone-options): Move
'make-clone-options-bytestructure' call within 'lambda*' body.

git/clone.scm
@@ -50,12 +50,13 @@ on failure."
         (pointer->repository! (dereference-pointer out))))))
 
 (define make-clone-options
-  (let ((proc (libgit2->procedure* "git_clone_init_options" `(* ,unsigned-int)))
-        (clone-options (make-clone-options-bytestructure)))
+  (let ((proc (libgit2->procedure* "git_clone_init_options"
+                                   `(* ,unsigned-int))))
     (lambda* (#:key (fetch-options (make-fetch-options)))
-      (proc (clone-options->pointer clone-options) CLONE-OPTIONS-VERSION)
-      (set-clone-options-fetch-opts! clone-options fetch-options)
-      clone-options)))
+      (let ((clone-options (make-clone-options-bytestructure)))
+        (proc (clone-options->pointer clone-options) CLONE-OPTIONS-VERSION)
+        (set-clone-options-fetch-opts! clone-options fetch-options)
+        clone-options))))
 
 (define clone-init-options
   ;; Deprecated alias for compatibility with 0.2.