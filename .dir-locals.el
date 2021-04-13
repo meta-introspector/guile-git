2e3ed788948a34068c0f8a6dcbfb283b37809a26
Ludovic Courtès <ludo@gnu.org>
2021-04-13 15:53:21 +0200

    maint: Adjust '.dir-locals.el'.
    * .dir-locals.el: Change "nil" to "scheme-mode" so we can have tabs in
    makefiles.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,7 @@
 ;; The 'nil' configuration applies to all modes.
-((nil . ((indent-tabs-mode . nil)
-         (tab-width . 2)
-         (eval . (progn
-                   (put 'with-directory 'scheme-indent-function 1)
-                   (put 'with-repository 'scheme-indent-function 2))))))
+((scheme-mode
+  . ((indent-tabs-mode . nil)
+     (tab-width . 8)
+     (eval . (progn
+               (put 'with-directory 'scheme-indent-function 1)
+               (put 'with-repository 'scheme-indent-function 2))))))
