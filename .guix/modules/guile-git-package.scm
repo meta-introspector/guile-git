8b936fb3d5104672c2d4375be4de8b82cbf50e56
Ludovic Courtès <ludo@gnu.org>
2024-02-24 18:29:32 +0100

    build: Turn ‘guix.scm’ into a module under ‘.guix/modules’.
    * guix.scm: Move to…
    * .guix/modules/guile-git-package.scm: … here.  Add license header.
    Turn into a module.
    * guix.scm: Make a symlink to the file above.
    * Makefile.am (EXTRA_DIST): Add them.

---
+++ .guix/modules/guile-git-package.scm
@@ -0,0 +1,65 @@
+;;; Guile-Git --- GNU Guile bindings of libgit2
+;;; Copyright © 2016 Amirouche Boubekki <amirouche@hypermove.net>
+;;; Copyright © 2016, 2017 Erik Edrosa <erik.edrosa@gmail.com>
+;;; Copyright © 2024 Ludovic Courtès <ludo@gnu.org>
+;;;
+;;; This file is part of Guile-Git.
+;;;
+;;; Guile-Git is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; Guile-Git is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with Guile-Git.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (guile-git-package)
+  #:use-module ((guix licenses) #:select (gpl3+))
+  #:use-module (guix gexp)
+  #:use-module ((guix git-download) #:select (git-predicate))
+  #:use-module (guix packages)
+  #:use-module (guix build-system gnu)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages ssh)
+  #:use-module (gnu packages texinfo)
+  #:use-module (gnu packages tls)
+  #:use-module (gnu packages version-control))
+
+(define %srcdir
+  (string-append (dirname (current-filename)) "/../.."))
+
+(define-public guile-git
+  (package
+    (name "guile-git")
+    (version "0.1")
+    (source (local-file %srcdir "guile-git-checkout"
+                        #:recursive? #t
+                        #:select? (git-predicate %srcdir)))
+    (build-system gnu-build-system)
+    (native-inputs
+     `(("autoconf" ,autoconf)
+       ("automake" ,automake)
+       ("pkg-config" ,pkg-config)
+       ("texinfo" ,texinfo)
+       ("openssh" ,openssh)
+       ("git" ,git)))
+    (inputs
+     `(("guile" ,guile-3.0)
+       ("libgit2" ,libgit2)
+       ("openssl" ,openssl)
+       ("zlib" ,zlib)
+       ("guile-bytestructures" ,guile-bytestructures)))
+    (synopsis "Guile bindings for libgit2")
+    (description "")
+    (home-page "")
+    (license gpl3+)))
+
+guile-git
